7a7cb641ea5deea579ad506c1942a1bb33db533e
Charlene <charlenecalderon14@gmail.com>
2023-05-04 21:18:52 -0700

    insert node function: inserts node to specific part of fucntion

--- CSC340Project/Source.cpp
+++ CSC340Project/Source.cpp
@@ -62,6 +62,61 @@ public:
 		this->tail = nullptr;
 	}
 
+	//insert
+    void insert (T data){
+        Node<T>* newNode = new Node <T>(data);
+        if(head == nullptr){
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        if (data < head -> getData())
+        {
+            newNode -> setNextNode(head);
+            head -> setPreviousNode(newNode);
+            head = newNode;
+            return;
+        }
+        if(data > tail -> getData ()){
+            newNode->setPreviousNode(tail);
+            tail->setNextNode(newNode);
+            tail = newNode;
+            return;
+        }
+		
+        /*
+        if(data == 0){
+            newNode -> setNextNode(head);
+            head = newNode;
+            return;
+        }
+        */
+
+        int i;
+        Node<T>* temp = head;
+        while (temp->getNextNode() != nullptr && temp->getNextNode()->getData() < data){
+            temp = temp -> getNextNode();
+            i++;
+        }
+
+        /*
+        int i = 0;
+        for (int i = 0; i < data; i++){
+            temp = temp -> getNextNode();
+            if (temp == nullptr);{
+                std::cout << "Index out of range" << std::endl;
+                return;
+
+            }
+        */
+        newNode->setNextNode(temp->getNextNode());
+        temp->setNextNode(newNode);
+        newNode->setPreviousNode(temp);
+        newNode->getNextNode()->setPreviousNode(newNode);
+    }
+
+	
+
 	void add(T data) {
 		Node<T>* newNode = new Node<T>(data);
 
