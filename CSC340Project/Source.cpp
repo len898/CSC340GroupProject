55c36712fe1c4f20549789bbea47d200fe915cf4
lennart@familie-richter.de <lennart@familie-richter.de>
2023-05-08 23:33:15 -0700

    Finished the merging. still working on the test

--- CSC340Project/Source.cpp
+++ CSC340Project/Source.cpp
@@ -84,38 +84,39 @@ public:
 		}
 	}
 
-    void insert (T data){
-        Node<T>* newNode = new Node<T>(data);
-        if(head == nullptr){
-            head = newNode;
-            tail = newNode;
-            return;
-        }
-        if (data <= head->getData()) {
-            newNode -> setNextNode(head);
-            head -> setPrevNode(newNode);
-            head = newNode;
-            return;
-        }
-        if(data >= tail->getData()){
-            newNode->setPrevNode(tail);
-            tail->setNextNode(newNode);
-            tail = newNode;
-            return;
-        }
-        Node<T>* temp = head;
-        while (temp->getNextNode() != nullptr && temp->getNextNode()->getData() < data){
-            temp = temp -> getNextNode();
-            
-        }
-    
-        newNode->setNextNode(temp->getNextNode());
-        temp->setNextNode(newNode);
-        newNode->setPrevNode(temp);
-        newNode->getNextNode()->setPrevNode(newNode);
-    }
+	//Update with binary search
+	void insert(T data) {
+		Node<T>* newNode = new Node<T>(data);
+		if (head == nullptr) {
+			head = newNode;
+			tail = newNode;
+			return;
+		}
+		if (data <= head->getData()) {
+			newNode->setNextNode(head);
+			head->setPrevNode(newNode);
+			head = newNode;
+			return;
+		}
+		if (data >= tail->getData()) {
+			newNode->setPrevNode(tail);
+			tail->setNextNode(newNode);
+			tail = newNode;
+			return;
+		}
+		Node<T>* temp = head;
+		while (temp->getNextNode() != nullptr && temp->getNextNode()->getData() < data) {
+			temp = temp->getNextNode();
 
-	
+		}
+
+		newNode->setNextNode(temp->getNextNode());
+		temp->setNextNode(newNode);
+		newNode->setPrevNode(temp);
+		newNode->getNextNode()->setPrevNode(newNode);
+	}
+
+
 
 	void add(T data) {
 		Node<T>* newNode = new Node<T>(data);
@@ -123,7 +124,8 @@ public:
 		if (head == nullptr) {
 			head = newNode;
 			tail = newNode;
-		} else {
+		}
+		else {
 			tail->setNextNode(newNode);
 			newNode->setPrevNode(tail);
 			tail = newNode;
@@ -137,10 +139,12 @@ public:
 				if (nodeToDelete == head) {
 					head = head->getNextNode();
 					head->setPrevNodeNull();
-				} else if (nodeToDelete == tail) {
+				}
+				else if (nodeToDelete == tail) {
 					tail = tail->getPrevNode();
 					tail->setNextNodeNull();
-				} else {
+				}
+				else {
 					Node<T>* prevNode = nodeToDelete->getPrevNode();
 					Node<T>* nextNode = nodeToDelete->getNextNode();
 					prevNode->setNextNode(nextNode);
@@ -166,16 +170,39 @@ public:
 		return nullptr;
 	}
 
-	void print() {
+	std::string print() {
 		Node<T>* temp = head;
+		std::string output = "";
 
 		while (temp != nullptr) {
+			auto val = temp->getData();
+			output += std::to_string(val);
 			std::cout << temp->getData() << " ";
 			temp = temp->getNextNode();
 		}
 
-		std::cout << std::endl;
+		//std::cout << std::endl;
+		return output;
 	}
+
+	//Modified the list from which it was called
+	//List 2 does not need to be sorted
+	//Calling list will be modified and sorted
+	//Second list remains unchanged
+	void mergeLists(const LinkedList<T>* listTwo) {
+		if (!this->head && !listTwo->head) {
+			return;
+		}
+		else {
+			Node<T>* temp = listTwo->head;
+			while (temp != nullptr) {
+				this->insert(temp->getData());
+				temp = temp->getNextNode();
+			}
+		}
+	}
+
+	//Append function possible
 
 private:
 	Node<T>* head;
@@ -236,9 +263,47 @@ void testSearch() {
 	}
 }
 
+bool emptyMerge() {
+	LinkedList<int> list1 = LinkedList<int>();
+	LinkedList<int> list2 = LinkedList<int>();
+	list1.mergeLists(&list2);
+	bool returnVal = list1.print() == "";
+	return returnVal;
+}
+
+bool callingListEmptyMerge() {
+	LinkedList<int> list1 = LinkedList<int>();
+	LinkedList<int> list2 = LinkedList<int>();
+	list2.add(25);
+	list1.mergeLists(&list2);
+	std::string expect_output = "25 ";
+	bool returnVal = list1.print() == expect_output;
+	return returnVal;
+}
+
+void testMerge() {
+	if (emptyMerge()) {
+		std::cout << "Expected Output" << std::endl;
+		std::cout << "Empty Merge Functional" << std::endl;
+	}
+	else {
+		std::cout << "Unexpected Output" << std::endl;
+		std::cout << "Empty Merge Not Functional" << std::endl;
+	}
+
+	if (callingListEmptyMerge()) {
+		std::cout << "Expected Output" << std::endl;
+		std::cout << "Calling List Empty Merge Functional" << std::endl;
+	}
+	else {
+		std::cout << "Unexpected Output" << std::endl;
+		std::cout << "Calling List Empty Merge Not Functional" << std::endl;
+	}
+}
+
 int main(int argc, const char* argv[]) {
 
-	LinkedList<int>* list = new LinkedList<int>();
+	/*LinkedList<int>* list = new LinkedList<int>();
 	LinkedList<std::string>* stringList = new LinkedList<std::string>();
 
 	list->add(3);
@@ -252,9 +317,10 @@ int main(int argc, const char* argv[]) {
 
 	stringList->add("Hello");
 	stringList->add("Team");
-	stringList->print();
+	stringList->print();*/
 
-	testSearch();
+	//testSearch();
+	testMerge();
 
 	return 0;
 }
